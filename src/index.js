90ab9014b1e036289488451b0fb8eba33f8b31ff
ksetrin <ksetrinn@gmail.com>
2017-09-20 02:06:32 +0500

    add Redux for app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+
+import './styles/index.css';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
